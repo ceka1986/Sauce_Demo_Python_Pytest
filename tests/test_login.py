06843e217dddd4d48a2b81140e0031a3ea043289
ceka1986 <cekicmilovan@gmail.com>
2026-03-25 21:32:19 +0100

    Test: add first valid login test case

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,7 +8,7 @@ class TestLogin:
 
     def test_valid_login(self, login_page:LoginPage, inventory_page:InventoryPage):
         login_page.open()
-        login_page.login_with_credentials("standarad_user", "secret_sauce")
+        login_page.login_with_credentials("standard_user", "secret_sauce")
 
         assert inventory_page.get_title() == "Products"
 
