4f3edd462061302dc5824a548d819ff883e5aa02
ceka1986 <cekicmilovan@gmail.com>
2026-04-04 10:06:24 +0200

    fix: finalize requirements and chrome options for CI

--- conftest.py
+++ conftest.py
@@ -23,6 +23,9 @@ def driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920,1080")
+
+    chrome_options.add_argument("--disable-gpu")  
+    chrome_options.add_argument("--remote-allow-origins=*")
     
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
